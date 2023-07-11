edd7b8c5110b29561118c30322ee71ca361ff4b3
ArthurBatista <arthurbat13.as@gmail.com>
2023-07-10 23:41:23 -0300

    random number

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var intervalId;
-var updateInterval =2000 ; 
-var updateCount =0 ; 
+var updateInterval =2000 ;
+var updateCount =0 ;
 
 function generateRandomNumber() {
-   var maxValue = document.getElementById("maxValue").value; 
-   var randomNumber = Math.floor(Math.random() * maxValue) +1 ; 
-   document.getElementById("randomNumber").innerHTML = randomNumber; 
+   var maxValue = document.getElementById("maxValue").value;
+   var randomNumber = Math.floor(Math.random() * maxValue) +1 ;
+   document.getElementById("randomNumber").innerHTML = randomNumber;
 }
 
 function startAutoUpdate() {
    if (intervalId) {
        clearInterval(intervalId);
    }
-   updateInterval =2000 ; 
-   updateCount =0 ; 
+   updateInterval =2000 ;
+   updateCount =0 ;
    intervalId = setInterval(updateRandomNumber, updateInterval);
 }
 
 function stopAutoUpdate() {
    if (intervalId) {
        clearInterval(intervalId);
-       intervalId = null; 
-       updateInterval =2000 ; 
-       updateCount =0 ; 
+       intervalId = null;
+       updateInterval =2000 ;
+       updateCount =0 ;
    }
 }
 
@@ -32,7 +32,7 @@ function updateRandomNumber() {
    updateCount++;
    
    if (updateCount %2 ===0 && updateInterval >1400 ) {
-       updateInterval -=100 ; 
+       updateInterval -=100 ;
        clearInterval(intervalId);
        intervalId = setInterval(updateRandomNumber, updateInterval);
    }
